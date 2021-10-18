7940e04613099665cbe0e7de75f1abe202354f2a
Chairullatif Aji Sadewa <khoirullatif337@gmail.com>
2021-10-18 13:40:38 +0700

    add expected exception for Unit Testing:

--- app/src/test/java/com/khoirullatif/barvolume/MainViewModelTest.kt
+++ app/src/test/java/com/khoirullatif/barvolume/MainViewModelTest.kt
@@ -7,9 +7,14 @@ import org.junit.Rule
 import org.junit.rules.ExpectedException
 import kotlin.jvm.Throws
 import org.junit.Assert.*
+import java.lang.NumberFormatException
 
 class MainViewModelTest {
     private lateinit var mainViewModel: MainViewModel
+
+    //anotasi for thrown variable can running before testing run
+    @get:Rule
+    var thrown = ExpectedException.none()
 
     @Before
     fun init() {
@@ -23,5 +28,16 @@ class MainViewModelTest {
         val height = "3"
         mainViewModel.calculate(width, height, length)
         assertEquals(6, mainViewModel.result)
+    }
+
+    @Test
+    @Throws(NumberFormatException::class)
+    fun doubleInputCalculateTest() {
+        val width = "1"
+        val length = "2.4"
+        val height = "3"
+        thrown.expect(NumberFormatException::class.java)
+        thrown.expectMessage("For input string: \"2.4\"")
+        mainViewModel.calculate(width, height, length)
     }
 }
